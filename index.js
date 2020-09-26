c6372cb8b2e811f67b10b999ab0f6447a1309f90
JT802 <jonathan.tresch@gmail.com>
2020-09-25 22:12:48 -0400

    refactored/reorg/prettified for resubmission

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function ask(questionText) {
 initialize(); //this allows a player to set a min and max
 
 async function initialize() {
+  console.log("\n") //There are quite a few console.log("\n") written throughout. I know it seems verbose, but it is just my attempt to prettify terminal output.
   let userChoice = await ask("Would you like to play a game where the computer (A) guesses the number, or the human (B) does? ")
   if (userChoice === "A") {
     compGame();
@@ -24,48 +25,54 @@ async function initialize() {
 
 
 async function compGame() {
-
+  console.log("\n");
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
+  console.log("\n");
 
-  let userMin = await ask("What number do you want for the minimum? ");
+  let userMin = await ask("What number do you want for the minimum? ")
+  console.log("\n");
   while (isNaN(parseInt(userMin))) {
-    userMin = await ask("Please enter numbers only.")
+    userMin = await ask("Please enter numbers only. ")
   }
-  let userMax = await ask("What number do you want for the maximum? ");
+  let userMax = await ask("What number do you want for the maximum? ")
+  console.log("\n");
   while (isNaN(parseInt(userMax))) {
-    userMin = await ask("Please enter numbers only.")
+    userMax = await ask("Please enter numbers only. ")
   }
   let min = parseInt(userMin);
   let max = parseInt(userMax);
   let guessComp = (Math.floor(Math.random() * (max - min + 1)) + min); //Random number generator (able to ask user to set min and max)
 
-  let compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: "); { //revised logic to be more specific
+  let compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: "); //revised logic to be more specific
 
-    console.log(compQuestion);
-    while (compQuestion !== "Y" || compQuestion !== "y") {
-      let higherLower = await ask("Is the number (H)igher or (L)ower: ");
-      if (higherLower === "H" || higherLower === "h") {
-        min = guessComp;
-        guessComp = (Math.floor(Math.random() * (max - min + 1)) + min)
-        compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: ");
-      } else if (higherLower === "L" || higherLower === "l") {
-        max = guessComp;
-        guessComp = (Math.floor(Math.random() * (max - min + 1)) + min);
-        compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: ");
-      } else {
-        console.log("Tsk. Tsk. No cheating!")
-      }
+  while (compQuestion === "N" || compQuestion === "n") { //fixed argument here due to mixing up !== and === conditionals
+    console.log("\n")
+    let higherLower = await ask("Is the number (H)igher or (L)ower: ");
+    if (higherLower === "H" || higherLower === "h") {
+      console.log("\n")
+      min = guessComp;
+      guessComp = (Math.floor(Math.random() * (max - min + 1)) + min)
+      compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: ");
+    } else if (higherLower === "L" || higherLower === "l") {
+      console.log("\n")
+      max = guessComp;
+      guessComp = (Math.floor(Math.random() * (max - min + 1)) + min);
+      compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: ");
+    } else {
+      console.log("Tsk. Tsk. No cheating!")
     }
-    console.log("Awesome. Humans are cool! So are bow-ties and fezzes. Thanks for playing!")
-    process.exit();
   }
+  console.log('\n');
+  console.log("Awesome. Humans are cool! So are bow-ties and fezzes. Thanks for playing!")
+  console.log("\n")
+  process.exit();
 }
 
 
 async function humanGame() {
-
+  console.log("\n")
   console.log("Let's play a game where I (computer) make up a number, and you (human) try to guess it.");
-
+  console.log("\n")
 
   let min = 1;
   let max = 100;
@@ -73,15 +80,21 @@ async function humanGame() {
 
   let humanQuestion = await ask("What do you think my secret number is? ");
 
+  while (isNaN(parseInt(humanQuestion))) {
+    humanQuestion = await ask("Please enter numbers only. ")
+  }
+
   while (parseInt(humanQuestion) !== guessHuman) { //this forces input to be a numeral
+    console.log("\n")
     if (humanQuestion > guessHuman) {
       humanQuestion = await ask("Your guess is too high. Please try again. ")
     } else if (humanQuestion < guessHuman) {
       humanQuestion = await ask("Your guess is too low. Please try again. ")
     }
   }
-
+  console.log("\n")
   console.log("Awesome. Computers rule! Thanks for playing!")
+  console.log("\n")
   process.exit();
 
 }
