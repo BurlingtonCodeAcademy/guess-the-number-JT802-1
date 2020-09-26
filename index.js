9ed09c590936d0c107d322fc3b7b8c3cc506b6c1
JT802 <jonathan.tresch@gmail.com>
2020-09-25 20:50:55 -0400

    Refactored and reorg for resubmission

--- index.js
+++ index.js
@@ -7,82 +7,81 @@ function ask(questionText) {
   });
 }
 
-initialize();
+initialize(); //this allows a player to set a min and max
 
 async function initialize() {
-    let userChoice = await ask("Would you like to play a game where the computer (A) guesses the number, or the human (B) does? ")
-    if (userChoice === "A") {
-        compGame();
-    } else if (userChoice === "B") {
-        humanGame();
-    } else {
-        console.log("Silly human! Please try again.")
-        initialize();
-    }
+  let userChoice = await ask("Would you like to play a game where the computer (A) guesses the number, or the human (B) does? ")
+  if (userChoice === "A") {
+    compGame();
+  } else if (userChoice === "B") {
+    humanGame();
+  } else {
+    console.log("Silly human! Please try again.")
+    initialize();
+  }
 };
 
-compGame();
+
 
 async function compGame() {
 
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
 
-  let min = 1;
-  let max = 100;
-  let guessComp = (Math.floor(Math.random() * (max - min + 1)) + min); //Random number generator for range 1-100 (Too limiting?)
+  let userMin = await ask("What number do you want for the minimum? ");
+  while (isNaN(parseInt(userMin))) {
+    userMin = await ask("Please enter numbers only.")
+  }
+  let userMax = await ask("What number do you want for the maximum? ");
+  while (isNaN(parseInt(userMax))) {
+    userMin = await ask("Please enter numbers only.")
+  }
+  let min = parseInt(userMin);
+  let max = parseInt(userMax);
+  let guessComp = (Math.floor(Math.random() * (max - min + 1)) + min); //Random number generator (able to ask user to set min and max)
 
-  let compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: "); { //Why does a typed response of "y" or "n" (lowercase-y or -n) make the game quit? (see lines 52-53) Getting lowercase-y or -n back seems to be the only instance where this occurs.
+  let compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: "); { //revised logic to be more specific
 
     console.log(compQuestion);
-    while (compQuestion === "N") {
+    while (compQuestion !== "Y" || compQuestion !== "y") {
       let higherLower = await ask("Is the number (H)igher or (L)ower: ");
-      if (higherLower === "H") {
+      if (higherLower === "H" || higherLower === "h") {
         min = guessComp;
         guessComp = (Math.floor(Math.random() * (max - min + 1)) + min)
         compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: ");
-      } else if (higherLower === "L") {
+      } else if (higherLower === "L" || higherLower === "l") {
         max = guessComp;
         guessComp = (Math.floor(Math.random() * (max - min + 1)) + min);
         compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: ");
       } else {
-        higherLower = await ask("Is the number (H)igher or (L)ower: ");
         console.log("Tsk. Tsk. No cheating!")
-      } //Not sure the else (anti-cheat) is ever hit, even though I think its scope is correct.
-    } 
+      }
+    }
     console.log("Awesome. Humans are cool! So are bow-ties and fezzes. Thanks for playing!")
     process.exit();
   }
 }
 
-humanGame();
 
 async function humanGame() {
 
   console.log("Let's play a game where I (computer) make up a number, and you (human) try to guess it.");
 
+
   let min = 1;
   let max = 100;
-  let guessHuman = (Math.floor(Math.random() * (max - min + 1)) + min); //Same random number generator as before.
+  let guessHuman = (Math.floor(Math.random() * (max - min + 1)) + min); //same number generator used previously
 
-  let humanQuestion = await ask("Is my secret number: " + guessHuman + "?" + "\n(Y)es or (N)o: "); { //Why does a typed response of "y" or "n" (lowercase-y or -n) make the game quit? (see lines 85-86) Getting lowercase-y or -n back seems to be the only instance where this occurs.
+  let humanQuestion = await ask("What do you think my secret number is? ");
 
-      console.log(humanQuestion);
-      while (humanQuestion === "N") {
-          let higherLower = await ask("Is the number (H)igher or (L)ower: ");
-          if (higherLower === "H") {
-              min = guessHuman;
-              guessHuman = (Math.floor(Math.random() * (max - min + 1)) + min)
-              humanQuestion = await ask("Is the secret number: " + guessHuman + "?" + "\n(Y)es or (N)o: ");
-          } else if (higherLower === "L") {
-              max = guessHuman;
-              guessHuman = (Math.floor(Math.random() * (max - min + 1)) + min);
-              humanQuestion = await ask("Is the secret number: " + guessHuman + "?" + "\n(Y)es or (N)o: ");
-          } else {
-              higherLower = await ask("Is the number (H)igher or (L)ower: ");
-              console.log("Tsk. Tsk. No cheating!")
-          } //Not sure the else (anti-cheat) is ever hit, even though I think its scope is correct.
-      }
-      console.log("Awesome. Computers rule! Thanks for playing!")
-      process.exit();
+  while (parseInt(humanQuestion) !== guessHuman) { //this forces input to be a numeral
+    if (humanQuestion > guessHuman) {
+      humanQuestion = await ask("Your guess is too high. Please try again. ")
+    } else if (humanQuestion < guessHuman) {
+      humanQuestion = await ask("Your guess is too low. Please try again. ")
+    }
   }
+
+  console.log("Awesome. Computers rule! Thanks for playing!")
+  process.exit();
+
 }
