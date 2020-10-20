f8f93426d7fde4a1561e80786910f8fc4932ffa9
JT802 <jonathan.tresch@gmail.com>
2020-10-20 08:58:12 -0400

    Project resubmission 10/20/20

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function ask(questionText) {
   });
 }
 
-initialize(); //this allows a player to set a min and max
+initialize(); // When the game starts, player is asked to choose their game mode.
 
 async function initialize() {
-  console.log("\n") //There are quite a few console.log("\n") written throughout. I know it seems verbose, but it is just my attempt to prettify terminal output.
+  console.log("\n") // There are quite a few console.log("\n") written throughout. I know it seems verbose, but it is just my attempt to make the terminal output easier to read.
   let userChoice = await ask("Would you like to play a game where the computer (A) guesses the number, or the human (B) does? ")
   if (userChoice === "A") {
     compGame();
@@ -25,7 +25,7 @@ async function initialize() {
 
 
 async function compGame() {
-  console.log("\n");
+  console.log("\n"); // In this game, the player sets the min and max for the computer's guessing.
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
   console.log("\n");
 
@@ -36,14 +36,14 @@ async function compGame() {
   }
   let userMax = await ask("What number do you want for the maximum? ")
   console.log("\n");
-  while (isNaN(parseInt(userMax))) {
+  while (isNaN(parseInt(userMax))) { // Forces input to accept only numerals
     userMax = await ask("Please enter numbers only. ")
   }
   let min = parseInt(userMin);
   let max = parseInt(userMax);
   let guessComp = (Math.floor(Math.random() * (max - min + 1)) + min); //Random number generator (able to ask user to set min and max)
 
-  let compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: "); //revised logic to be more specific
+  let compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: "); //revised the logic to be more specific
 
   while (compQuestion === "N" || compQuestion === "n") { //fixed argument here due to mixing up !== and === conditionals
     console.log("\n")
@@ -55,7 +55,7 @@ async function compGame() {
       compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: ");
     } else if (higherLower === "L" || higherLower === "l") {
       console.log("\n")
-      max = guessComp;
+      max = guessComp; // I was unable to implement a binary search tree.
       guessComp = (Math.floor(Math.random() * (max - min + 1)) + min);
       compQuestion = await ask("Is your secret number: " + guessComp + "?" + "\n(Y)es or (N)o: ");
     } else {
@@ -70,7 +70,7 @@ async function compGame() {
 
 
 async function humanGame() {
-  console.log("\n")
+  console.log("\n") // This is the reverse game where the human tries to guess the computer's secret number
   console.log("Let's play a game where I (computer) make up a number, and you (human) try to guess it.");
   console.log("\n")
 
@@ -84,7 +84,7 @@ async function humanGame() {
     humanQuestion = await ask("Please enter numbers only. ")
   }
 
-  while (parseInt(humanQuestion) !== guessHuman) { //this forces input to be a numeral
+  while (parseInt(humanQuestion) !== guessHuman) { //this forces input to be a numeral, as in prior game
     console.log("\n")
     if (humanQuestion > guessHuman) {
       humanQuestion = await ask("Your guess is too high. Please try again. ")
@@ -92,7 +92,7 @@ async function humanGame() {
       humanQuestion = await ask("Your guess is too low. Please try again. ")
     }
   }
-  console.log("\n")
+  console.log("\n") // That's all, folks.
   console.log("Awesome. Computers rule! Thanks for playing!")
   console.log("\n")
   process.exit();
